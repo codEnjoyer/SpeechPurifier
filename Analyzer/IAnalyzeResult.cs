e57c0d8e6bf09d6a6a3b246cd6e483690283f581
codEnjoyer <ch4shkin.nikita@gmail.com>
2024-01-26 19:28:41 +0500

    Добавляет интерфейс IAnalyzeResult.cs

---
+++ Analyzer/IAnalyzeResult.cs
@@ -0,0 +1,9 @@
+﻿using SpeechPurifier.Mistakes;
+
+namespace SpeechPurifier.Analyzer;
+
+public interface IAnalyzeResult
+{
+    public IReadOnlyList<IMistake> Mistakes { get; }
+    public int PurityScore { get; }
+}
